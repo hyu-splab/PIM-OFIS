d35d45ed7990e842ba1050f596fc6c6e9c120bb3
hyojune1025 <63467255+hyojune1025@users.noreply.github.com>
2025-12-10 13:58:04 +0900

    Update dataset download URLs to new paths

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-wget https://splab.hanyang.ac.kr/archive/PG_dataset.tar.gz
+wget https://splab.hanyang.ac.kr/archive/pim/PG_dataset.tar.gz
 tar -zxvf PG_dataset.tar.gz
 rm PG_dataset.tar.gz
 
-wget https://splab.hanyang.ac.kr/archive/spmv_dcsr256_dataset.tar.gz
+wget https://splab.hanyang.ac.kr/archive/pim/spmv_dcsr256_dataset.tar.gz
 tar -zxvf spmv_dcsr256_dataset.tar.gz
 rm spmv_dcsr256_dataset.tar.gz
 
-wget https://splab.hanyang.ac.kr/archive/spmv_dcsr_dataset.tar.gz
+wget https://splab.hanyang.ac.kr/archive/pim/spmv_dcsr_dataset.tar.gz
 tar -zxvf spmv_dcsr_dataset.tar.gz
 rm spmv_dcsr_dataset.tar.gz
 
-wget https://splab.hanyang.ac.kr/archive/spmv_rbdcsr512_dataset.tar.gz
+wget https://splab.hanyang.ac.kr/archive/pim/spmv_rbdcsr512_dataset.tar.gz
 tar -zxvf spmv_rbdcsr512_dataset.tar.gz
 rm spmv_rbdcsr512_dataset.tar.gz
 
-wget https://splab.hanyang.ac.kr/archive/spmv_rbdcsr_dataset.tar.gz
+wget https://splab.hanyang.ac.kr/archive/pim/spmv_rbdcsr_dataset.tar.gz
 tar -zxvf spmv_rbdcsr_dataset.tar.gz
 rm spmv_rbdcsr_dataset.tar.gz
